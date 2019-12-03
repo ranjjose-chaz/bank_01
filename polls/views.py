9fc57e274baa8f68fdd2f46cdfd176d8b2ec672b
ranjjose-chaz <ranjjose@gmail.com>
2019-12-03 23:40:54 +0530

    std err msgs

--- polls/views.py
+++ polls/views.py
@@ -14,11 +14,11 @@ import jwt
 
 
 # Standard Err Responses
-NO_DATA_MATCHED = {error: {{message: "No item matched"}}}
-TOKEN_INVALID = {error: {{message: "JWT token invalid"}}}
-TOKEN_EXPIRED = {error: {{message: "JWT token expired"}}}
-TOKEN_MISSING = {error: {{message: "JWT token missing"}}}
-INVALID_PARAMS = {error: {{message: "The request can't be processed"}}} 
+NO_DATA_MATCHED = {"error": {{"message": "No item matched"}}}
+TOKEN_INVALID = {"error": {{"message": "JWT token invalid"}}}
+TOKEN_EXPIRED = {"error": {{"message": "JWT token expired"}}}
+TOKEN_MISSING = {"error": {{"message": "JWT token missing"}}}
+INVALID_PARAMS = {"error": {{"message": "The request can't be processed"}}} 
 
 
 def validate_jwt(f):    
