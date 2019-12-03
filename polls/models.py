389d7a17fa0661ed6a81f5b9a6b1529b60055e20
ranjjose-chaz <ranjjose@gmail.com>
2019-12-03 16:45:59 +0530

    fixed model - added primary key into id field

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.db import models
 
 class Banks(models.Model):
-    id = models.BigIntegerField()	
+    id = models.BigIntegerField(primary_key=True)	
     name = models.CharField(max_length=49)
 
     class Meta:
